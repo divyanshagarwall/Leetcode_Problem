f93eb30f8d2d5acc97192de73e598918f6d2244f
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2022-12-11 14:09:45 +0530

    Time: 57 ms (5.30%), Space: 52.1 MB (5.12%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,17 +14,32 @@
  * }
  */
 class Solution {
-    int maxSum = Integer.MIN_VALUE;
-    public int maxPathSum(TreeNode root) {
-        findMaxPathSum(root);
-        return maxSum;
+    public Iterable<TreeNode> topSort(TreeNode root) {
+    Deque<TreeNode> result = new LinkedList<>();
+    if (root != null) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            result.push(curr);
+            if (curr.right != null) stack.push(curr.right);
+            if (curr.left != null) stack.push(curr.left);
+        }
     }
-    public int findMaxPathSum(TreeNode root) {
-         if(root == null) return 0;   
-         int left =   Math.max(0, findMaxPathSum(root.left));   //why compare with 0
-        //if 3,-12,-2 so findmax =-12 instead of returning -12 return 0 so comp with 0
-         int right =  Math.max(0, findMaxPathSum(root.right));        
-         maxSum = Math.max(maxSum , left + right + root.val);
-         return Math.max(left, right) + root.val;
+    return result;
+}
+    
+    public int maxPathSum(TreeNode root) {
+        int result = Integer.MIN_VALUE;
+    Map<TreeNode, Integer> maxRootPath = new HashMap<>(); // cache
+    maxRootPath.put(null, 0); // for simplicity we want to handle null nodes
+    for (TreeNode node : topSort(root)) {
+        // as we process nodes in post-order their children are already cached
+        int left = Math.max(maxRootPath.get(node.left), 0);
+        int right = Math.max(maxRootPath.get(node.right), 0); 
+        maxRootPath.put(node, Math.max(left, right) + node.val);
+        result = Math.max(left + right + node.val, result);
+    }
+    return result;
     }
 }
