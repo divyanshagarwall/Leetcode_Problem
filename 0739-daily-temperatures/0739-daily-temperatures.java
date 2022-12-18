03d757423b15dff3ecdc0627d5208b2e63ca3f54
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2022-12-18 09:40:36 +0530

    Time: 46 ms (84.21%), Space: 54.6 MB (88.91%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        if(length == 0) return null;
+        
+        int[] answer = new int[length];
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i=length-1; i>=0; i--){
+            int temperature = temperatures[i];
+            map.put(temperature, i);
+            while(!deque.isEmpty() && deque.peekLast()<=temperature){
+                deque.pollLast();
+            }
+            
+            int result = deque.isEmpty() ? -1 : deque.peekLast();
+            if(result == -1){
+                answer[i] = 0;
+            }
+            else{
+                result = map.get(result)-i;
+                answer[i] = result;
+            }
+            
+            deque.addLast(temperatures[i]);
+            
+        }
+        
+        return answer;
+    }
+}
